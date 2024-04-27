e64295243aca8e547cd747ade16091ceb536971f
cys4585 <dev.youngsu@gmail.com>
2024-04-27 20:40:11 +0900

    deploy: step2 재배포

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import CardEnrollForm from "./components/payment/CardEnrollForm";
 
 function App() {
   return (
-    <BrowserRouter basename={"/react-payments"}>
+    <BrowserRouter basename={"/react-payments/"}>
       <Routes>
         <Route path={"/"} element={<CardEnrollComplete />} />
         <Route path={"/card-enroll-complete"} element={<CardEnrollForm />} />
